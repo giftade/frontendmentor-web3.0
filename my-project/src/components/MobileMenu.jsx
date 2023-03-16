90cb9c82de52fb0f840691abbf35c356d6f75474
Gift Segun Ade <giftsegunade@gmail.com>
2023-03-16 16:59:49 +0100

    worked on mobile menu

--- my-project/src/components/MobileMenu.jsx
+++ my-project/src/components/MobileMenu.jsx
@@ -3,7 +3,7 @@ import closeBtn from "../../../assets/images/icon-menu-close.svg";
 export const MobileMenu = () => {
   return (
     <div>
-      <div class="hidden">
+      <div class="md:hidden">
         <div
           id="menu"
           className="flex flex-col bg-offWhite right-0 top-0 h-screen w-3/5 fixed py-5 pr-2text-left z-10"
